15c50460cb904f67e21159ab529c63e7522f78f8
unknown <yonaycl@gmail.com>
2017-12-21 12:12:09 +0000

    adding migrations

---
+++ src/main/resources/db/migration/V1_1__createAnimeTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE anime
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    posterImage TEXT NOT NULL,
+    title TEXT NOT NULL,
+    year TEXT NOT NULL ,
+    rating TEXT NOT NULL,
+    video TEXT NOT NULL
+);
+
+INSERT INTO public.medicos(posterImage, title, year, rating, video)
+VALUES ('http://img1.ak.crunchyroll.com/i/spire2/3b86bbf3ad40f36f86df634e138837ca1484112439_full.jpg',
+ 'Fuuka',
+'2017',
+ '10',
+'');
